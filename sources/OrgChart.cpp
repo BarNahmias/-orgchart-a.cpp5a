f578fbc06ca4623e4c7f99ae2c8595621c3745bc
Bar Nahmias <92825016+BarNahmias@users.noreply.github.com>
2022-05-19 00:35:36 +0300

    Add files via upload

--- sources/OrgChart.cpp
+++ sources/OrgChart.cpp
@@ -5,56 +5,50 @@ using namespace ariel;
 namespace ariel {
 
     
-
+//  OrgChart method
 
     OrgChart OrgChart::add_root(string root) {return *this;}
 
     OrgChart OrgChart::add_sub(string a, string b) {return *this;}
 
-
     std::ostream &operator<<(ostream &output, const OrgChart &oc) {
-        return  output << "it";
+        return  output << "it"; }
 
-    }
+
+    //  begin & end iterator  method
 
     Iterator OrgChart::begin_level_order() {
-        Iterator it;
-        return it;
-    }
+        Iterator iterator;
+        return iterator;}
 
     Iterator OrgChart::end_level_order() {
-        Iterator it;
-        return it;
-    }
+        Iterator iterator;
+        return iterator;}
 
     Iterator OrgChart::begin_reverse_order() {
-        Iterator it;
-        return it;
-    }
+        Iterator iterator;
+        return iterator;}
 
     Iterator OrgChart::reverse_order() {
-        Iterator it;
-        return it;
-    }
+        Iterator iterator;
+        return iterator;}
 
     Iterator OrgChart::begin_preorder() {
-        Iterator it;
-        return it;
-    }
-    Iterator OrgChart::end_preorder() {
-        Iterator it;
-        return it;
-    }
+        Iterator iterator;
+        return iterator;}
 
-    Iterator OrgChart::begin() {
-        Iterator it;
-        return it;
-    }
+
+    Iterator OrgChart::end_preorder() {
+        Iterator iterator;
+        return iterator;}
+
+    Iterator OrgChart::begin()  {
+        Iterator iterator;
+        return iterator;}
 
     Iterator OrgChart::end() {
-        Iterator it;
-        return it;
-    }
+        Iterator iterator;
+        return iterator;}
 
 
 
